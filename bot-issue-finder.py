35414e3bc884ab86b2783266699d5673fab27c79
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-08-10 12:16:14 +0200

    Additional Error Handling
    Now catches Badcredentialerrors (likely occur when a repo has been renamed) and skips those repositories.
    When any other error occurs, the process is stopped and the results are output earlier (rather than completely discarded).

--- bot-issue-finder.py
+++ bot-issue-finder.py
@@ -4,7 +4,7 @@ import logging
 import os
 
 from datetime import datetime, timedelta
-from github import Github, enable_console_debug_logging
+from github import Github, enable_console_debug_logging, BadCredentialsException
 from github.GithubObject import _NotSetType as NotSet
 from math import inf
 from rate_limit_retry import rate_limited_retry_search
@@ -227,39 +227,50 @@ if __name__ == "__main__":
     print(f"Repo Filtering was started at {repo_start_time}! \nNB: This might take an even longer while, so grab two drinks and relax twice as much!\n")
 
     repos = {}
-    with open(settings.get('results-output-file'), newline='', encoding='utf-8') as issue_file:
-        csv_reader = csv.DictReader(issue_file)
-        for row in csv_reader:
-            repo_name = row['repo']
-            if repo_name not in repos:
-                repo = run_repo_query(repo_name)
+    try:
+        with open(settings.get('results-output-file'), newline='', encoding='utf-8') as issue_file:
+            csv_reader = csv.DictReader(issue_file)
+            for row in csv_reader:
+                repo_name = row['repo']
+                if repo_name not in repos:
+                    try:
+                        repo = run_repo_query(repo_name)
 
-                if repo_adheres_to_settings(repo, settings):
-                    repos[repo_name] = {
-                        'issues': [],
-                        'stars': repo.stargazers_count,
-                        'forks': repo.forks_count,
-                        'watchers': repo.watchers_count,
-                        'is_fork': repo.fork,
-                        'is_private': repo.private,
-                        'is_archived': repo.archived,
-                        'estimated_size': repo.size,
-                        'created_at': repo.created_at.isoformat(),
-                        'updated_at': repo.updated_at.isoformat(),
-                        'skipped': False,
-                    }
-                else:
-                    repos[repo_name] = {
-                        'skipped': True,
-                    }
+                        if repo_adheres_to_settings(repo, settings):
+                            repos[repo_name] = {
+                                'issues': [],
+                                'stars': repo.stargazers_count,
+                                'forks': repo.forks_count,
+                                'watchers': repo.watchers_count,
+                                'is_fork': repo.fork,
+                                'is_private': repo.private,
+                                'is_archived': repo.archived,
+                                'estimated_size': repo.size,
+                                'created_at': repo.created_at.isoformat(),
+                                'updated_at': repo.updated_at.isoformat(),
+                                'skipped': False,
+                                'error': False,
+                            }
+                        else:
+                            repos[repo_name] = {
+                                'skipped': True,
+                                'error': False,
+                            }
+                    except BadCredentialsException:
+                        repos[repo_name] = {
+                                'skipped': True,
+                                'error': True,
+                            }
 
-            if not repos[repo_name]['skipped']:
-                repos[repo_name]['issues'].append({'number': row['number'], 'created_at': row['created_at']})
+                if not repos[repo_name]['skipped']:
+                    repos[repo_name]['issues'].append({'number': row['number'], 'created_at': row['created_at']})
+    except Exception as e:
+        print(f"Unexpected Exception! {e}")
 
     output_filename = settings.get('results-repo-output-file')
     with open(output_filename, 'w', newline='', encoding='utf-8') as output_file:
         output_file.write(json.dumps(repos))
 
     repo_end_time = datetime.now()
-    print(f"\nSearch was ended at {repo_end_time}, and took {repo_end_time - repo_end_time} h:mm:ss!")
+    print(f"\nSearch was ended at {repo_end_time}, and took {repo_end_time - repo_start_time} h:mm:ss!")
     print(f"Obtained {len(repos)} unique repositories, which were output in {output_filename}!")
